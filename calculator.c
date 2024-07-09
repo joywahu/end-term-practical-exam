f6ed49c2dbc2be88eb6236061cfaf8411d49770e
joywahu <wahujoy6@gmail.com>
2024-07-09 13:06:36 +0300

    end term exam

---
+++ calculator.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+int main() {
+
+}
+
+char operator;
+double firstNumber, secondNumber, results;
+printf( firsNumber, secondNumber, results);
+ 
+//  user operator
+
+    printf("enter an operators, (+, -, *, / ) : ");
+scanf("%c"&operators);
+
+printf("enter two operands" );
+scanf(%lf %lf, firstNumber, secondNumber,  results);
+
+switch(operators);
+
+    case'-';
+
+int printf("firstNumber - secondNumber" );
+
+int scanf(".%2lf - .%2lf", );
+
+case'+';
+
+int printf("firstNumber + secondNumber" );
+int scanf(".%2lf + .%2lf": );
+
+case'*';
+
+int printf("firstNumber * secondNumber" );
+int scanf(".%2lf * .%2lf": );
+
+case'/';
+
+if(secondNumber ! = 0);
+
+    else(firstNumber, secondNumber, results);
+    printf("firstNumber / secondNumber" );
+    scanf(".%2lf /.%2lf":);
+
+    // defining an error
+
+    if(firstNumber !) = 0;{
+
+    else(secondNumber, firstNumber, results);
+
+    printf("secondNumber / firsNumber" );
+scanf(".%2lf + .%2lf" );
+
+
+}
+
+return = 0;
+
+
